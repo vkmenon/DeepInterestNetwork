754f513a8895d946c2523ed61e7076c1c9733488
Vivek <mod.x07@gmail.com>
2018-09-24 16:21:26 -0700

    add infer.py
    script to run inference on test data

---
+++ din/infer.py
@@ -0,0 +1,37 @@
+"""
+Usage: python infer.py --bs=<batch_size>    
+"""
+
+import argparse
+import time
+import pickle
+import tensorflow as tf
+from input import DataInput, DataInputTest
+from model import Model
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--bs",type=int)
+args = parser.parse_args()
+
+
+
+with open('dataset.pkl', 'rb') as f:
+  train_set = pickle.load(f)
+  test_set = pickle.load(f)
+  cate_list = pickle.load(f)
+  user_count, item_count, cate_count = pickle.load(f)
+
+model = Model(user_count, item_count, cate_count, cate_list)
+
+test_batch_size = args.bs
+
+t1=time.time()
+
+with tf.Session() as sess:
+    model.restore(sess,'save_path/ckpt')
+    for k, uij in DataInputTest(test_set, test_batch_size):
+        model.test(sess,uij)
+
+t2=time.time()
+
+print ("Total Eval Time: " + str(t2-t1))
